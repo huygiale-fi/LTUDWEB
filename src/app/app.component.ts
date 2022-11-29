e1c868d24bf23c285f62f5a1de8540faded0a818
huygiale <huygiale2202@gmail.com>
2022-11-29 16:15:22 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { ChildrenOutletContexts, NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
 import { slideInAnimation } from './animation';
 declare let gtag: Function;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,25 +16,28 @@ declare let gtag: Function;
 export class AppComponent {
   constructor(private contexts: ChildrenOutletContexts,
     private router: Router) {
-
   }
-  
+
   ngOnInit() {
     this.setUpAnalytics();
   }
+
   getRouteAnimationData() {
     return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
   }
+
   title = 'LTHD-FE';
 
   setUpAnalytics() {
     this.router.events.pipe(filter(event => event instanceof NavigationEnd))
       .subscribe((event: any) => {
+        console.log(event.urlAfterRedirects)
         gtag('config', 'G-CS300YPFLY',
           {
-            page_path: event.urlAfterRedirects
+            'page_path': event.urlAfterRedirects
           }
         );
-      });
+      }
+    );
   }
 }
